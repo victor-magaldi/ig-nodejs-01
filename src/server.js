895fe1f2b45d71c6b971442fcf36631b6b5a631f
victor-magaldi-acct <victmagaldi@hotmail.com>
2023-02-08 20:42:48 -0300

    structure

--- src/server.js
+++ src/server.js
@@ -1,9 +1,51 @@
 import http from "node:http";
 
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+const users = [];
+
 const server = http.createServer((req, res) => {
-  console.log("teste");
-  return res.end("hello world");
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
+    return res.end("create user");
+  }
+
+  return res.end("Hello");
 });
 
-server.listen(4000);
-console.log("teste1");
+server.listen(400);
