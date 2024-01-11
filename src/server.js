32139b802111d0c82381d50acc391d069434afd0
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-11 20:58:16 -0300

    create account

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
-import express from "express";
+import express, { response } from "express";
+import { v4 as uuidV4 } from "uuid";
+// v4 numeros randomicos
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidV4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
 
 app.listen(9000);
