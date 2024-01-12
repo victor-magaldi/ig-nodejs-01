57299e5fd7565f33cfda0334fe3b3172871471a6
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-12 19:27:08 -0300

    fix middleware

---
+++ src/middleware/verifyIfExistsAccountByCpf.js
@@ -0,0 +1,14 @@
+import { customers } from "../customers.js";
+
+export function verifyIfExistsAccountByCpf(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return res.status(400).json({ error: "User not found" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
