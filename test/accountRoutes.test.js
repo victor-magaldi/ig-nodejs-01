1bae01ebd23527bfe0b8a1ac4a7016ac36b5a972
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-31 18:57:14 -0300

    modularize folder test

--- test/accountRoutes.test.js
+++ test/accountRoutes.test.js
@@ -1,27 +1,25 @@
 import { expect } from "chai";
 import supertest from "supertest";
-import app from "./src/server.js";
+import app from "../src/server.js";
 
 const request = supertest(app);
 
 describe("Testes de API", () => {
-  it("Deve criar uma nova conta", async () => {
+  it("Create account - POST /account", async () => {
     const response = await request
       .post("/account")
       .send({ cpf: "12345678901", name: "Novo Cliente" });
 
     expect(response.status).to.equal(201);
+    expect(response.body).to.be.empty;
   });
 
-  it("search account", async () => {
+  it("Search account - GET /account", async () => {
     const response = await request.get("/account").set("cpf", "12345678901");
-
     expect(response.status).to.equal(200);
     expect(response.body).to.have.property("cpf");
     expect(response.body).to.have.property("name");
     expect(response.body).to.have.property("id");
     expect(response.body).to.have.property("statements");
   });
-
-  // Adicione mais testes conforme necessário
 });
